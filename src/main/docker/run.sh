01c33cb31e531f8470c15fb5b0d86b0266c59bf1
liaolangsheng <liaolangsheng@163.com>
2018-12-17 16:22:55 +0800

    增加环境变量输出

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "$MY_HELLO,$MY_KEY"
+
 java -Djava.security.egd=file:/dev/./urandom \
      -jar /usr/local/deploy/@project.build.finalName@.jar
